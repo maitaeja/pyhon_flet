e6154fe78808719b7d6e31b25592180f6a605c07
maitaeja <maita.eja@gmail.com>
2026-08-20 12:29:07 -0500

    Configure application to use environment port
    Added port configuration for the application.

--- main.py
+++ main.py
@@ -23,7 +23,10 @@ def main(page: ft.Page):
     page.update()
 
 if __name__ =="__main__":
-    ft.app(target=main)
-
-
+    port = int(os.environ.get("PORT", 8080))
+    ft.app(target=main,
+           #view = ft.AppView.WEB_BROWSER,
+           #host = "0.0.0.0",
+           #port = port
+           )
 
